f6a0a4b3bba6581637b3dfa759374c695fb32f82
Dean Chen <dean.chen@duke.edu>
2011-03-06 18:51:07 -0500

    Fixed bug with import assigning poinrt(-90 0) to all points and implemented initial version of distance filter algorithm.

--- application/models/db_model.php
+++ application/models/db_model.php
@@ -19,25 +19,67 @@ class Db_model extends CI_Model {
     // convert string to function call, if string is null or not well formatted
     // set as null
     
-    $coord = NULL;
+    $coord_array = NULL;
     if ($coord_string != NULL and $coord_string != '') 
     {
       $coord_array = explode(", ", $coord_string);
-      if (sizeof($coord == 2)) { // make sure we have two coordinates
-        $coord = NULL;
-      }
+      if (sizeof($coord_array) != 2) $coord_array = NULL;
     } 
     
-    if ($coord == NULL) {
+    if ($coord_array == NULL) {
       // set to antarctica
       $coord_array = array(-90, 0); 
     }
-
+    print_r($coord_array);
+    print("<br />");
     $coord = "GeomFromText('POINT(" . implode(' ', $coord_array) . ")')";
     
     // insert in to db
     $this->db->set($record);
-    if ($coord) $this->db->set('coordinate', $coord, FALSE);
+    $this->db->set('coordinate', $coord, FALSE);
     $this->db->insert('point');
+  }
+  
+  /**
+   * Return records according to distance
+   */
+  function get_records($center_array = NULL, $distance = NULL)
+  {
+    /**
+     * Query for points inside square bounding box and then filter points
+     * too far away
+     * 
+     * http://howto-use-mysql-spatial-ext.blogspot.com/
+     */
+    
+    if (sizeof($center_array) == 2 && $distance) 
+    {
+      $center = "GeomFromText('POINT(" . implode(' ', $center_array) . ")')";
+      $bounding_box = "CONCAT('POLYGON((', 
+        X($center) - $distance, ' ', Y($center) - $distance, ',', 
+        X($center) + $distance, ' ', Y($center) - $distance, ',', 
+        X($center) + $distance, ' ', Y($center) + $distance, ',', 
+        X($center) - $distance, ' ', Y($center) + $distance, ',', 
+        X($center) - $distance, ' ', Y($center) - $distance, '))' 
+        )
+      ";
+      
+      $sql = "SELECT name, AsText(coordinate), SQRT(POW( ABS( X(coordinate) - X($center)), 2) + POW( ABS(Y(coordinate) - Y($center)), 2 )) AS distance 
+              FROM point 
+              WHERE Intersects( coordinate, GeomFromText($bounding_box) ) 
+              AND SQRT(POW( ABS( X(coordinate) - X($center)), 2) + POW( ABS(Y(coordinate) - Y($center)), 2 )) < $distance 
+              ORDER BY distance;";
+      $query = $this->db->query($sql);
+      
+      print("<pre>");
+      print_r($query->result());
+      print("</pre>");
+    } 
+    else 
+    {
+      print "need more paramters";
+    }
+    
+    
   }
 }
