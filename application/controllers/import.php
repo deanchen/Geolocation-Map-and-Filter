5f1fbcbd101293ea1398f6aff332473bbc6b9c43
Dean Chen <dean.chen@duke.edu>
2011-03-06 17:58:09 -0500

    Completed import controller and model.

---
+++ application/controllers/import.php
@@ -0,0 +1,44 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Import extends CI_Controller {
+
+  function __construct()
+  {
+    parent::__construct();
+  }
+
+  function index()
+  {
+    echo "Go to /import_and_remove_csv/csvfile.csv";
+  }
+  
+  /**
+   * Read in a correctly formatted csv file and import contents in to database 
+   * using db_model and delete the csv file afterwards to reduce the chance of
+   * double import
+   */
+  function import_and_remove_csv($filename)
+  {
+    $fields = array();
+    $records = array();  
+    $header = true;
+    $count = 0;
+    if (($handle = fopen(getenv("DOCUMENT_ROOT") . "/$filename", "r")) !== FALSE) {
+      while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        if (!$header) {
+          for ($c=0; $c < count($data); $c++) {
+              $record[$fields[$c]] = $data[$c];
+          }
+          
+          $this->db_model->insert_record($record);
+          $count++;
+        } else {
+          $fields = $data;
+          $header = false;
+        }   
+      }
+      fclose($handle);
+    }
+    echo "Successfully inserted $count records";
+  }
+}
--- application/models/db_model.php
+++ application/models/db_model.php
@@ -4,9 +4,35 @@ class Db_model extends CI_Model {
   
   function __construct()
   {
-      // Call the Model constructor
-      parent::__construct();
+    // Call the Model constructor
+    parent::__construct();
   }
   
-  
+  function insert_record($record)
+  {
+    /*
+     * convert coordinate string to mysql geospacial fucntion call
+     */
+    $coord_string = $record['coordinates'];
+    unset($record['coordinates']);
+    
+    // convert string to function call, if string is null or not well formatted
+    // set as null
+    $coord = NULL;
+    if ($coord_string != NULL and $coord_string != '') 
+    {
+      $coord_array = explode(", ", $coord_string);
+      
+      if (sizeof($coord == 2)) { // make sure we have two coordinates
+        $coord = "GeomFromText('POINT(" . implode(' ', $coord_array) . ")')";
+      } else {
+        $coord = NULL;
+      }
+    }
+    
+    // insert in to db
+    $this->db->set($record);
+    if ($coord) $this->db->set('coordinate', $coord, FALSE);
+    $this->db->insert('point');
+  }
 }
